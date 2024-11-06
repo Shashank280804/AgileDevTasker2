0fbead71dee742be7917320a18580e670050f43c
shashank <116015929+Shashank280804@users.noreply.github.com>
2024-11-06 13:29:01 +0530

    Try fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://agiledevtasker2-backend.onrender.com",
         changeOrigin: true,
       },
     },
